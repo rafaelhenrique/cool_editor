1b36f568fd20c3ebf6c2157a13473bc74c250e68
Rafael Henrique da Silva Correia <rafael@abraseucodigo.com.br>
2016-05-14 22:16:10 -0300

    Mock redis_store on test

--- cool_editor/core/tests/test_core_view.py
+++ cool_editor/core/tests/test_core_view.py
@@ -1,10 +1,13 @@
 import pytest
 from flask import url_for
+from mock import patch
 
 
 @pytest.mark.usefixtures('client_class')
 class TestCoreView:
 
-    def test_get_status_code(self):
+    @patch('cool_editor.redis_store')
+    def test_get_status_code(self, mock_redis_store):
+        mock_redis_store.hset.return_value = True
         response = self.client.get(url_for('core.index'))
         assert response.status_code == 200
