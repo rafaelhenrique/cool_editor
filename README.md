48951d053f316bad68db285cafa5784e5efbcc82
Rafael Henrique da Silva Correia <rafael@abraseucodigo.com.br>
2016-05-14 21:21:51 -0300

    Add frontend of editor

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 # Cool Editor
 
 The (cool)aborative editor writen in Python.
+
+## Front-end theme (css and js)
+
+Credits for: https://github.com/Dsyko/kanso-codemirror
+
--- cool_editor/core/__init__.py
+++ cool_editor/core/__init__.py
@@ -1,5 +1,6 @@
 from flask import Blueprint
 
-core = Blueprint('core', __name__, template_folder='templates')
+core = Blueprint('core', __name__, template_folder='templates',
+                 static_folder='static', static_url_path='core')
 
 from ..core import views
